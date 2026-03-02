4c371e479497e9a20814aa420835e60003e9c6f9
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2026-03-02 16:13:56 +0300

    Init commit

---
+++ src/main/java/com/ramis/selfdeploy/SelfdeployApplication.java
@@ -0,0 +1,13 @@
+package com.ramis.selfdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramis/selfdeploy/SelfdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramis.selfdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
